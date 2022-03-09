2b66b05237ae8e0568ceda8e39e14d41f0040126
ERLANRAHMAT <erlanrahmatdwisaputra@gmail.com>
2022-03-09 16:36:25 +0700

    Apikey

---
+++ shop/donasi.js
@@ -0,0 +1,18 @@
+const donasibot = () => {
+return `DONASI BISA VIA E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Minta Ke Owner
+2. OVO
+	ㅁ Minta Ke Owner
+3. DANA
+	ㅁ Minta Ke Owner
+	
+
+`
+	}
+
+exports.donasibot = donasibot
+ 
---
+++ shop/sewa.js
@@ -0,0 +1,25 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 5.000
+⬡ 1 BULAN =9.000
+⬡ MANEN =15.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 15K
+
+
+⬣ MINAT? PM
+⬡ wa.me/6285803583481
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
